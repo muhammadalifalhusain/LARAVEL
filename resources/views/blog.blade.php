40a187baf9c44732bff3e6c63003e187b46d24e8
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-11-18 21:21:56 +0700

    install tailwind

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum tempore aspernatur quo vel voluptas, ipsa consequatur quos nemo ipsam earum dolorum, minus rem veritatis iste impedit voluptates! Eius, perspiciatis minus.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/blog', function () {
+    return view('blog');
+});
