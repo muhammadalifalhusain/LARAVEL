d3a02058fa83124e2df61db23dcc53b469afbe35
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-11-21 21:34:26 +0700

    add content article_blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -13,4 +13,30 @@
 
     
   </article>
+
+  <article class="py-8 max-w-screen-md border-gray-200">
+    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-800">Article1</h2>
+    <div class=" text-base text-gray-500">
+      <a href="#">Muhammad Alif al Husain</a> | 15 November 2024
+    </div>
+      <p class="my-4 font-light">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?
+      </p>
+      <a href="#" class="font-medium text-blue-500 hover:underline">ReadMore &raquo;</a>
+
+    
+  </article>
+
+  <article class="py-8 max-w-screen-md border-gray-200">
+    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-800">Article1</h2>
+    <div class=" text-base text-gray-500">
+      <a href="#">Muhammad Alif al Husain</a> | 15 November 2024
+    </div>
+      <p class="my-4 font-light">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?
+      </p>
+      <a href="#" class="font-medium text-blue-500 hover:underline">ReadMore &raquo;</a>
+
+    
+  </article>
 </x-layout>
