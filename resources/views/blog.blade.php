f7d5c839a8907552fc9a4386f3bcebe28ce44c61
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-11-21 19:59:36 +0700

    update blog-page

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,16 @@
 <x-layout>
 <x-slot:title>{{$title}}</x-slot:title>
-  <h2>Ini Halaman BlogPage</h2>
+
+  <article class="py-8 max-w-screen-md border-gray-200">
+    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-800">Article1</h2>
+    <div class=" text-base text-gray-500">
+      <a href="#">Muhammad Alif al Husain</a> | 21 November 2024
+    </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?
+      </p>
+      <a href="#">ReadMore &raquo;</a>
+
+    
+  </article>
 </x-layout>
