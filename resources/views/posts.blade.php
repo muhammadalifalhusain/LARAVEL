62d940b851e87f1c63a554689f1eee4be1a3c762
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-11-24 19:43:38 +0700

    view data route

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,42 +1,18 @@
 <x-layout>
 <x-slot:title>{{$title}}</x-slot:title>
 
-  <article class="py-8 max-w-screen-md border-gray-200">
-    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-800">Article1</h2>
-    <div class=" text-base text-gray-500">
-      <a href="#">Muhammad Alif al Husain</a> | 21 November 2024
-    </div>
-      <p class="my-4 font-light">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?
-      </p>
-      <a href="#" class="font-medium text-blue-500 hover:underline">ReadMore &raquo;</a>
-
-    
-  </article>
+@foreach ($posts as $post)
 
   <article class="py-8 max-w-screen-md border-gray-200">
-    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-800">Article1</h2>
+    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-800">{{$post ['title']}}</h2>
     <div class=" text-base text-gray-500">
-      <a href="#">Muhammad Alif al Husain</a> | 15 November 2024
+      <a href="#">{{$post ['author']}}</a> | 21 November 2024
     </div>
       <p class="my-4 font-light">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?
+        {{$post ['body']}}
       </p>
       <a href="#" class="font-medium text-blue-500 hover:underline">ReadMore &raquo;</a>
-
-    
+ 
   </article>
-
-  <article class="py-8 max-w-screen-md border-gray-200">
-    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-800">Article1</h2>
-    <div class=" text-base text-gray-500">
-      <a href="#">Muhammad Alif al Husain</a> | 15 November 2024
-    </div>
-      <p class="my-4 font-light">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?
-      </p>
-      <a href="#" class="font-medium text-blue-500 hover:underline">ReadMore &raquo;</a>
-
-    
-  </article>
+  @endforeach
 </x-layout>
