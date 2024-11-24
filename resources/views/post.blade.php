a8cfa672c3f48fc00b3c9fdbfe130ee08f487b28
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-11-24 20:12:06 +0700

    add fitur readmore

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,20 +1,19 @@
 <x-layout>
 <x-slot:title>{{$title}}</x-slot:title>
 
-@foreach ($posts as $post)
+
 
   <article class="py-8 max-w-screen-md border-gray-200">
-    <a href="/posts/{{$post ['id']}}" class = "hover:underline">
     <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-800">{{$post ['title']}}</h2>
     </a>
     <div class=" text-base text-gray-500">
       <a href="#">{{$post ['author']}}</a> | 21 November 2024
     </div>
       <p class="my-4 font-light">
-        {{ Str::limit($post ['body'],100)}}
+        {{ $post ['body']}}
       </p>
-      <a href="/posts/{{$post ['id']}}" class="font-medium text-blue-500 hover:underline">ReadMore &raquo;</a>
+      <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo;Kembali ke Blog </a>
  
   </article>
-  @endforeach
+  
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
 
 Route::get('/', function () {
     return view('welcome', ['title' => 'HomePage']);
@@ -15,18 +16,21 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', ['title' => 'BlogPage', 'posts' => [
     [    
+        'id' => 1,
         'title' => 'Judul Artikel1',
         'author' => 'Muhammad Alif al Husain',
         'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
     
     ],
     [    
+        'id' => 2,
         'title' => 'Judul Artikel2',
         'author' => 'Muhammad Alif al Husain',
         'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
     
     ],
     [    
+        'id' => 3,
         'title' => 'Judul Artike3',
         'author' => 'Muhammad Alif al Husain',
         'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
@@ -35,6 +39,38 @@ Route::get('/posts', function () {
 
 
         ]]);
+});
+
+Route::get('/posts/{id}', function ($id){
+    $posts = 
+    [
+    [    
+        'id' => 1,
+        'title' => 'Judul Artikel1',
+        'author' => 'Muhammad Alif al Husain',
+        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
+    
+    ],
+    [    
+        'id' => 2,
+        'title' => 'Judul Artikel2',
+        'author' => 'Muhammad Alif al Husain',
+        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
+    
+    ],
+    [    
+        'id' => 3,
+        'title' => 'Judul Artike3',
+        'author' => 'Muhammad Alif al Husain',
+        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
+    
+    ]
+];
+
+    $post = Arr::first($posts, function($post) use ($id){
+        return $post['id'] == $id;
+    });
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
