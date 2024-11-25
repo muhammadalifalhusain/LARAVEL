b224ae8b12c7a519830b7e7a4f0f90b10db62fad
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-11-25 23:53:45 +0700

    add model blog

--- routes/web.php
+++ routes/web.php
@@ -2,39 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
+use App\Models\Post;
 
 
-class Post{
-    public static function allData(){
-        return [
-            [    
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel1',
-                'author' => 'Muhammad Alif al Husain',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
-            
-            ],
-            [    
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel2',
-                'author' => 'Muhammad Alif al Husain',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
-            
-            ],
-            [    
-                'id' => 3,
-                'slug' => 'judul-artikel-3',
-                'title' => 'Judul Artike3',
-                'author' => 'Muhammad Alif al Husain',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
-            
-            ],
-        ];
-         
-    }
-}
 
 Route::get('/', function () {
     return view('welcome', ['title' => 'HomePage']);
