2ca2b9984a9f3c8f019fb2ba57e3ea7368570900
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-11-25 22:37:27 +0700

    model data

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,39 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
 
+
+class Post{
+    public static function allData(){
+        return [
+            [    
+                'id' => 1,
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel1',
+                'author' => 'Muhammad Alif al Husain',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
+            
+            ],
+            [    
+                'id' => 2,
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel2',
+                'author' => 'Muhammad Alif al Husain',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
+            
+            ],
+            [    
+                'id' => 3,
+                'slug' => 'judul-artikel-3',
+                'title' => 'Judul Artike3',
+                'author' => 'Muhammad Alif al Husain',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
+            
+            ],
+        ];
+         
+    }
+}
+
 Route::get('/', function () {
     return view('welcome', ['title' => 'HomePage']);
 });
@@ -14,66 +47,13 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'BlogPage', 'posts' => [
-    [    
-        'id' => 1,
-        'slug' => 'judul-artikel-1',
-        'title' => 'Judul Artikel1',
-        'author' => 'Muhammad Alif al Husain',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
-    
-    ],
-    [    
-        'id' => 2,
-        'slug' => 'judul-artikel-2',
-        'title' => 'Judul Artikel2',
-        'author' => 'Muhammad Alif al Husain',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
-    
-    ],
-    [    
-        'id' => 3,
-        'slug' => 'judul-artikel-3',
-        'title' => 'Judul Artike3',
-        'author' => 'Muhammad Alif al Husain',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
-    
-    ],
-
-
-        ]]);
+    return view('posts', ['title' => 'BlogPage', 'posts' => Post::allData()
+        ]);
 });
 
 Route::get('/posts/{slug}', function ($slug){
-    $posts = 
-    [
-    [    
-        'id' => 1,
-        'slug' => 'judul-artikel-1',
-        'title' => 'Judul Artikel1',
-        'author' => 'Muhammad Alif al Husain',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
-    
-    ],
-    [    
-        'id' => 2,
-        'slug' => 'judul-artikel-2',
-        'title' => 'Judul Artikel2',
-        'author' => 'Muhammad Alif al Husain',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
-    
-    ],
-    [    
-        'id' => 3,
-        'slug' => 'judul-artikel-3',
-        'title' => 'Judul Artike3',
-        'author' => 'Muhammad Alif al Husain',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores modi officiis maxime itaque! Suscipit esse eligendi asperiores tenetur eum, velit maiores! Veniam, quo quam eaque sunt blanditiis laborum quidem?',   
-    
-    ]
-];
 
-    $post = Arr::first($posts, function($post) use ($slug){
+    $post = Arr::first(Post::allData(), function($post) use ($slug){
         return $post['slug'] == $slug;
     });
     return view('post', ['title' => 'Single Post', 'post' => $post]);
