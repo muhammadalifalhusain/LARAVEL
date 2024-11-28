8838065bcb2960de7693fb46a21c8ab80d37bf50
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-11-28 19:27:22 +0700

    belajar data dummy menggunakan tinker

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Arr;
  // Tambahkan baris ini
 
 class Post extends Model
 {
+    use HasFactory;
     protected $fillable = ['title', 'author', 'slug', 'body'];
     // Isi model Post
 }
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'title' => fake()->sentence(),
+            'author' => fake()->name(),
+            'slug' => Str::slug(fake()->sentence()),
+            'body' => fake()->text()
+        ];
+    }
+}
