23d43061398341e904464471fc43b6ad821d5842
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-12-03 19:02:39 +0700

    belajar seeder menggunakan model factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Post;
+use App\Models\Category;
+
+
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -15,9 +19,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Post::factory(100) -> recycle([
+            Category::factory(3) -> create(),
+            User::factory(5) -> create()
+        ]) -> create();
     }
 }
