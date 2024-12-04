a6122500a6eba567529e2e27c91e154725335a2d
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-12-04 20:19:34 +0700

    redesign single post blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,6 +40,7 @@ export default {
     },
     plugins: [
         require('flowbite/plugin'),
+        require("flowbite-typography"),
     ],
     safelist: ["bg-red-100", "bg-green-100", "bg-blue-100", "bg-yellow-100"],
 
